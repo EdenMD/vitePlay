b17d2584f055b058cb33437aecc4aa71b6f64a7f
EdenMD <techn2533@gmail.com>
2026-07-29 12:23:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ const config = {
         },
     },
     defaults: {
-        voice:              'bm_adam',
+        voice:              'am_adam',
         transition:         'fade',
         transitionDuration: 0.28,
     },
